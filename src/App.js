e16c16931891298af1562f6450e17630389dd75e
sandnb <nithinsandilyab@gmail.com>
2023-05-09 03:00:57 +0530

    here i have used proptypes and classnames and react-icons to build the buttons layout completely

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-
+import Button from "./Button";
+import { GoBell , GoCloudDownload, GoDatabase } from 'react-icons/go';
 
 function App() {
+
   return (
-    <div className="App">
-        <h1>Hello All</h1>
+    <div>
+       <div>
+        <Button success rounded outline >
+          <GoBell />
+          Click Me!!</Button> {/* text btw custom component is taken as value and added to the key named '{children}' by default as per JSX rule so we can use this prop children and can use it in parent component*/}
+       </div>
+       <div>
+        <Button danger outline>
+          <GoCloudDownload/>
+          Buy Now!</Button> {/* here as we know that these props are boolean so either T/F so when ever we mention the any prop in component means it is 'true' i.e, {primary:true}  */}
+       </div>
+       <div>
+        <Button warning outline>
+          <GoDatabase />
+          See Deal</Button> {/*so in JSX we simply need to pass the prop as it is but not with T/F value, if the prop is false then it will be considered as 'undefined' but not as 0*/}
+       </div>
+       <div>
+        <Button secondary outline>Hide ads!</Button>
+       </div>
+       <div>
+        <Button danger outline>Something</Button>
+       </div>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,55 @@
+// import PropTypes from "prop-types";
+import className from "classnames"; // this is used to define customised styles for the respective styles of buttons like primary, secondary, success, danger, warning etc.
+//use case of className function as an example;
+// const finalClassName = className('px-1.5', {  // here 1st arg is considered and applied to the component and  to the key whose value is true that style only are applied to the component, false values are omitted by the className function 
+//     'bg-blue-500' :true,
+//     'text-yellow-500': false, // here the key values are having dash in their name so we kept them in quotes as per JS rules
+// });
+
+// console.log(finalClassName); 
+
+function Button( {children,
+primary,
+secondary,                       // here all these props from primary to rounded are the styling of buttons that can be applied on buttons at different stages 
+success,                         // all these button styles are booleans and their purpose is if any one of the prop is 'true' then apply that styling to the Button and the catch here is that only one style should be applied.
+warning,                        // to check if applying of 2 styles to be eradicated we use PropTypes instead writing JS logic with logical operators
+danger,                         // as the proptypes is JS lib to validate the props that get passed into your component, if someone passes down the incorrect value then proptypes shows 'warning'.
+outline,
+rounded,
+...rest     // this means take all the props from the parent other than the mentioned props so it will take rest of the various eventHandlers from parent component and send to the button component instead of getting each and every eventHandlers
+} ) {
+    const classes = className(rest.className,'flex items-center px-3 py-1.5 mx-3 my-1.5 border' , { // these 1st arg style is gonna apply to the Button irrespective of the prop it contain and also rest.className ensures if nay className is defined on the Button then it is also included inside the classes.
+        'border-blue-500 bg-blue-500 text-white' :primary, // this style is only applied to Button if that Button has primary as prop
+        'border-gray-900 bg-gray-900 text-white' :secondary, // this style is only applied to Button if that Button has secondary as prop
+        'border-green-500 bg-green-500 text-white' :success, // this style is only applied to Button if that Button has success as prop
+        'border-yellow-400 bg-yellow-400 text-white' :warning, // this style is only applied to Button if that Button has warning as prop
+        'border-red-500 bg-red-500 text-white' :danger, // this style is only applied to Button if that Button has danger as prop
+        'rounded-full' : rounded,
+        'bg-light cream' : outline,
+        'text-blue-500' : outline && primary, // here if both are bool and are applied to the Button then if both are true then the latest style i.e, with style of outline will be applied to the Button;
+        'text-gray-900' : outline && secondary, // same explanation as above
+        'text-green-500' : outline && success, // same above explanation
+        'text-orange-400' : outline && warning, // same above explanation
+        'text-red-500' : outline && danger, // same above explanation
+    });
+
+    return <button {...rest}  className={classes}>{children}</button>// here we are taking the prop children from the App(parent) and using it here instead of hard coding in btw 'button' component.
+         // ...rest means take all the individual properties and values out of this object and assign all of them as props to the button element.
+}
+Button.propTypes ={
+    checkVariationValue: ( {primary,secondary,success,warning,danger} ) => {
+        const count = Number(!!primary) +  Number(!!secondary) +  Number(!!success) +  Number(!!warning) +  Number(!!danger); // above we've used JS Logic as where we convert the values of the styles(i.e, boolean) into number so if the value of prop we passed is true (ans = 1) if (ans = false) then (!!undefined) = 0 
+
+        if(count > 1) { // here if the 'count > 1' means that more than 1 style is applied to the Button as per above logic so we show a warning
+            return new Error('Only one of primary, secondary, success, warning, danger can be True')
+        }
+    } 
+
+   
+
+
+};
+
+
+
+export default Button;
